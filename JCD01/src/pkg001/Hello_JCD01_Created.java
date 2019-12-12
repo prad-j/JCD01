912a8d89802737cdf8c68a15e4ec6ecf4476345e
Pramods <Pramods@Pramod>
2019-12-12 19:15:17 +0530

    Start

---
+++ JCD01/src/pkg001/Hello_JCD01_Created.java
@@ -0,0 +1,7 @@
+package pkg001;
+
+public class Hello_JCD01_Created {
+		public static void main(String args[]){
+			System.out.println("Hi... JCD01 repository created."+ "\n" +"Now start coding.");
+		}
+}
