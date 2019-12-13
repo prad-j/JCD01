e09e116597dc2087fa46306f0341a79282e002c6
Pramods <Pramods@Pramod>
2019-12-14 01:28:02 +0530

    Boolean change value

---
+++ JCD01/src/pkg001/Boolean001.java
@@ -0,0 +1,14 @@
+package pkg001;
+
+public class Boolean001 {
+	public static void main(String args[]){
+		boolean a = true;
+		System.out.println("Value of a is "+a);
+		
+		boolean b = !a;
+		
+		//now value of boolean b will change to false
+		
+		System.out.println("Value of b is "+b);
+	}
+}
